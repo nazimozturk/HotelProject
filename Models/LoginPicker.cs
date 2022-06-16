5ad55169cc3f9d3328241128686c8b3b9417259e
Nazım ÖZTÜRK <Nazm.OZTURK@hotmail.com.tr>
2022-06-16 23:05:44 +0300

    Add project files.

---
+++ Models/LoginPicker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.Models
+{
+    public class LoginPicker
+    {
+        public Guid Id { get; set; } //Guidler unique değerler oluştururlar, bu sebeple Id için kullanılırlar.
+
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public string adultCount { get; set; } // dropdown
+        public string childCount { get; set; } // dropdown
+    }
+}
---
+++ Models/RoomList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HotelProject.Models
+{
+    public class RoomList
+    {
+        public static List<LoginPicker> _loginPickers = new List<LoginPicker>();
+    }
+}
